4bc4b6ba2c914b33f0e66bd9386de7d784fc4c09
ReneKubax <therene21@gmail.com>
2023-05-29 19:08:48 -0600

    config unrlencoded

--- index.js
+++ index.js
@@ -17,3 +17,4 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
