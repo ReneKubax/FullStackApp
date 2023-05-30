21560a83d80ec723dcd36f1c27026e658b4786be
ReneKubax <therene21@gmail.com>
2023-05-29 19:07:41 -0600

    config files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser, { json } from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -7,3 +7,13 @@ import multer from 'multer';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
